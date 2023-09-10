787f426758490c59fd95b4bacb35ac8791f79b49
Sarat Raj Sidharth <sidharthbatchu@gmail.com>
2023-09-10 14:31:10 -0700

    Create CPPWrap.cpp, basic code to run the binary with arguments.
    This code is supposed to run the binary file with arguments and break with exit code 1 when args aren't passed,  but yet needs some code to read the output from the execl command's usage/

---
+++ CPPWrap.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <unistd.h>
+#include <sys/wait.h>
+int main(int argc, char** argv) {
+ try{
+  Hex_Key = argv[1];
+  Hex_Text = argv[2];
+  throw 505;
+ }
+ catch(...){ cout << "Needs 2 args with execution to run.";}
+ // when code is compiled as g++ -o mother CPPWrap.cpp and 
+ // is supposed to receive executing command as ./mother arg1 arg2 ....
+ pid_t child_pid=fork();
+ if (child_pid==0){
+  execl("~/Downloads/CSE598_Project1_Handout/project1_handout","mothership","--key", Hex_Key, "--plaintext", Hex_Text, nullptr);
+  perror("execl");
+  exit(1);
+ }
+ else if(child_pid>0){
+ int status;
+ waitpid(child_pid, &status, 0);
+ if(WIFEXITED(status)){
+  iny exit_status = WEXITSTATUS(status);
+  std::cout << "Child process exited with status: " << exit_status << std::endl;
+ } 
+ else {
+ perror("fork");
+ return 1;
+ }
+ return 0;
+}
