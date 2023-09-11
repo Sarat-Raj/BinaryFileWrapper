d1af652777be95bba6937abd6f2f367a65197d69
Sarat Raj Sidharth <sidharthbatchu@gmail.com>
2023-09-10 19:29:25 -0700

    Update CPPWrap.cpp

--- CPPWrap.cpp
+++ CPPWrap.cpp
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <string.h>
+
 int main(int argc, char** argv) {
  try{
   std::string Hex_Key = argv[1];
@@ -11,9 +12,10 @@ int main(int argc, char** argv) {
  catch(...){ std::cout << "Needs 2 args with execution to run.";}
  // when code is compiled as g++ -o mother CPPWrap.cpp and 
  // is supposed to receive executing command as ./mother arg1 arg2 ....
- pid_t child_pid=fork();
+ 
+pid_t child_pid=fork();
  if (child_pid==0){
-  execl("/home/cen598/Downloads/CSE598_Project1_Handout/project1_handout","mothership","--key", Hex_Key, "--plaintext", Hex_Text, nullptr);
+  execl("/home/cen598/Downloads/CSE598_Project1_Handout/project1_handout/mothership","./mothership","--key", Hex_Key, "--plaintext", Hex_Text, nullptr); // parameters needed to be are complete details of the file - path to file including file name, name of the command,arguments untill they exhaust and ends with a null pointer. 
   perror("execl");
   exit(1);
  }
@@ -29,4 +31,4 @@ int main(int argc, char** argv) {
  return 1;
  }
  return 0;
-}
+ }
