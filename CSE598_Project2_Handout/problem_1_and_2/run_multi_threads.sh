46496810953c595a313d3e2d828cd5429044d7f5
Sarat Raj - Cen <sidharthbatchu@gmail.com>
2023-09-27 12:21:47 -0700

    updated the values with increased thread count

--- CSE598_Project2_Handout/problem_1_and_2/run_multi_threads.sh
+++ CSE598_Project2_Handout/problem_1_and_2/run_multi_threads.sh
@@ -1,4 +1,8 @@
-for threads in {1..16}; do
+
+# Clear existing data in the output file
+echo -n "" > recordThreadoutput.txt
+
+for threads in {1..25}; do
     export OMP_NUM_THREADS=$threads
     ./simple_stream >> recordThreadoutput.txt
 done
